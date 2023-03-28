56d336f244e77abd7ead0ab48663c6b90c6edaf9
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2023-03-28 23:35:39 +0000

    Add files via upload

---
+++ src/main/java/iliass/user/management/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package iliass.user.management.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
