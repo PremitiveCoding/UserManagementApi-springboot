062b0ef7d4d4c1780c9fa1889949cadd39a326f2
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2023-03-28 23:43:16 +0000

    Add files via upload

---
+++ src/main/java/me/iliass/usermanager/UserManager.java
@@ -0,0 +1,14 @@
+package me.iliass.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class UserManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(me.iliass.usermanager.UserManager.class, args);
+    }
+}
